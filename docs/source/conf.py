9703961742c217d251317f3a751478c7096f2cb0
Yashchuk Oleg <oazoer@gmail.com>
2014-05-05 02:09:26 +0400

    css 4

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -107,9 +107,8 @@ html_theme = 'default'
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
-html_theme_options = {
-  'cssfiles': ['extend.css'],
-}
+#html_theme_options = {}
+html_theme_options['cssfiles'] = ['_static/extend.css']
 
 # Add any paths that contain custom themes here, relative to this directory.
 #html_theme_path = []
